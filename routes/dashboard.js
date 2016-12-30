313b3e4a22c39880f3b27e6462e5582efc1de8ca
Rud156 <rudrangshu_1997@yahoo.com>
2016-12-30 11:11:30 +0530

    Load Documents

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,23 +6,38 @@ var Model = require('../Models/models');
 
 /* GET home page. */
 router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
-    res.render('dashboard');
+    var documents = res.locals.user.documents;
+    if (documents.length !== 0) {
+        var documentReq = documents[documents.length - 1];
+        Model.Docs.findOne({ hash: documentReq.hash }, function (err, docObj) {
+            if (err)
+                throw err;
+            if (!docObj) {
+                req.flash('errorMsg', 'Page does not exist');
+                res.redirect('/users/dashboard');
+            }
+            else {
+                var decipher = crypto.createDecipher('aes-256-ctr', res.locals.user.password);
+                var dec = decipher.update(docObj.content, 'hex', 'utf8');
+                dec += decipher.final('utf8');
+                docObj.content = dec;
+                res.render('dashboard', { docObj: docObj });
+            }
+        });
+    }
+    else
+        res.render('dashboard', { noObject: true });
 });
 
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
-    else {
-        req.flash('errorMsg', "Not Logged In");
-        res.redirect('/auth/login');
-    }
-}
-
-router.post('/save', function (req, res, next) {
+router.post('/save', ensureAuthenticated, function (req, res, next) {
     var title = req.body.title;
     var date = req.body.date;
     var hash = crypto.createHash('sha256').update(title + date + req.user.username).digest("hex");
     var userText = req.body.userText;
+
+    var cipher = crypto.createCipher('aes-256-ctr', res.locals.user.password);
+    userText = cipher.update(userText, 'utf8', 'hex');
+    userText += cipher.final('hex');
 
     var userDoc = {
         date: date,
@@ -58,23 +73,38 @@ router.post('/save', function (req, res, next) {
     });
 });
 
-router.get('/editor', function(req, res, next){
-    console.log("Activating the editor");
-    res.redirect('/users/dashboard');
+router.get('/editor', ensureAuthenticated, function (req, res, next) {
+    //console.log("Activating the editor");
+    res.render('dashboard', { editor: true });
 });
 
-router.route('/:id').get(function (req, res) {
-    Model.Docs.findOne({hash: req.params.id}, function(err, docObj){
-        if(err)
+router.route('/:id').get(ensureAuthenticated, function (req, res) {
+    Model.Docs.findOne({ hash: req.params.id }, function (err, docObj) {
+        if (err)
             throw err;
-        if(docObj){
-            res.render('dashboard', {docObj: docObj});
+        if (docObj) {
+            
+            var decipher = crypto.createDecipher('aes-256-ctr', res.locals.user.password);
+            var dec = decipher.update(docObj.content, 'hex', 'utf8');
+            dec += decipher.final('utf8');
+            docObj.content = dec;
+
+            res.render('dashboard', { docObj: docObj });
         }
-        else{
+        else {
             req.flash('errorMsg', 'Page does not exist');
             res.redirect('/users/dashboard');
         }
     });
 });
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    else {
+        req.flash('errorMsg', "Not Logged In");
+        res.redirect('/auth/login');
+    }
+}
 
 module.exports = router;
